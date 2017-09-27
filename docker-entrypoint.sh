8398b966b01258f3a73c3c03a7fd3e1954b24888
mr5 <u@mr-5.cn>
2017-09-27 14:23:01 +0800

    run stop before startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,5 +14,7 @@ echo ZK_TIMEOUT: $ZK_TIMEOUT
 echo $NID > /opt/otter_node/conf/nid
 sed -i s/"-server -Xms32m -Xmx3072m"/"-server -Xms32m -Xmx$MAX_MEN"/g /opt/otter_node/bin/startup.sh
 sed -ri "s/(otter.manager.address).*/\1 = $MANAGER_ADDRESS/" /opt/otter_node/conf/otter.properties
+
+/opt/otter_node/bin/stop.sh
 /opt/otter_node/bin/startup.sh
 tail -f /opt/otter_node/logs/manager.log & wait
